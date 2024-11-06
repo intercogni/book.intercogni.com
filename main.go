0fc52c7d31cb6d9d49c6997585f11309e48a622c
oportunitas <taibizzat165@gmail.com>
2024-11-06 12:18:59 +0700

    feat: array diagnostics

--- main.go
+++ main.go
@@ -5,9 +5,10 @@ import "fmt"
 func main() {
 	const conference_tickets = 50
 
-	var user_first_name string
-	var user_last_name string
+	var bookings [50]string
 	var user_email_addr string
+	var user_name_first string
+	var user_name_last string
 	var user_tickets int
 
 	conference_name := "Intercogni Monthly"
@@ -23,10 +24,10 @@ func main() {
 	fmt.Println("Get your tickets here to attend")
 
 	fmt.Printf("Please enter your first name: ")
-	fmt.Scan(&user_first_name)
-
+	fmt.Scan(&user_name_first)
 	fmt.Printf("Please enter your last name: ")
-	fmt.Scan(&user_last_name)
+	fmt.Scan(&user_name_last)
+	bookings[0] = user_name_first + " " + user_name_last
 
 	fmt.Printf("Please enter your email address: ")
 	fmt.Scan(&user_email_addr)
@@ -37,10 +38,14 @@ func main() {
 	remaining_tickets -= user_tickets
 
 	fmt.Printf(
-		"Successfully booked %v ticket/s for %v %v. Confirmation sent to %v \n",
-		user_tickets, user_first_name, user_last_name, user_email_addr)
+		"Successfully booked %v ticket/s for %v. Confirmation sent to %v \n",
+		user_tickets, bookings[0], user_email_addr)
 
 	fmt.Printf(
 		"%v tickets now remains for %v\n",
 		remaining_tickets, conference_name)
+
+	fmt.Printf("bookings array data: %v\n", bookings)
+	fmt.Printf("bookings array type: %T\n", bookings)
+	fmt.Printf("bookings array size: %v\n", len(bookings))
 }
