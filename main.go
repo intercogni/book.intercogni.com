e1f819699175287ddd1cf5ee8902a6c4ab422fb3
oportunitas <taibizzat165@gmail.com>
2024-11-06 11:45:56 +0700

    feat: variable definition

--- main.go
+++ main.go
@@ -5,4 +5,7 @@ import "fmt"
 func main() {
 	fmt.Println("Sign up for Intercogni's monthly conference on IoT and Robotics")
 	fmt.Println("Get your tickets here to attend")
+
+	var conference_name = "Intercogni Monthly"
+	fmt.Println(conference_name)
 }
