3ffbbaebb781f80862d40b6e7ac6b5741858fa2c
oportunitas <taibizzat165@gmail.com>
2024-11-06 11:47:24 +0700

    refactor: use variable for conference name

--- main.go
+++ main.go
@@ -3,9 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Sign up for Intercogni's monthly conference on IoT and Robotics")
-	fmt.Println("Get your tickets here to attend")
-
 	var conference_name = "Intercogni Monthly"
-	fmt.Println(conference_name)
+
+	fmt.Println("Sign up for", conference_name, "for IoT and Robotics")
+	fmt.Println("Get your tickets here to attend")
 }
