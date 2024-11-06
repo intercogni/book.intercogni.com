396bc6e40021e5a9a2b72332c0149bb958c91f2e
oportunitas <taibizzat165@gmail.com>
2024-11-06 11:49:27 +0700

    feat: vars and consts

--- main.go
+++ main.go
@@ -4,7 +4,13 @@ import "fmt"
 
 func main() {
 	var conference_name = "Intercogni Monthly"
+	const conference_tickets = 50
+	var remaining_tickets = 50
 
 	fmt.Println("Sign up for", conference_name, "for IoT and Robotics")
+	fmt.Println(
+		conference_tickets, "are given this month and", 
+		remaining_tickets, "are still available"
+	)
 	fmt.Println("Get your tickets here to attend")
 }
