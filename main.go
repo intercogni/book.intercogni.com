d90476097ca0be064b8a1fa9ff121a37287476c6
oportunitas <taibizzat165@gmail.com>
2024-11-06 12:27:19 +0700

    feat: loop ticket booking

--- main.go
+++ main.go
@@ -23,27 +23,30 @@ func main() {
 		conference_tickets, remaining_tickets)
 	fmt.Println("Get your tickets here to attend")
 
-	fmt.Printf("Please enter your first name: ")
-	fmt.Scan(&user_name_first)
-	fmt.Printf("Please enter your last name: ")
-	fmt.Scan(&user_name_last)
-	bookings = append(bookings, user_name_first+" "+user_name_last)
+	for {
+		fmt.Printf("Please enter your first name: ")
+		fmt.Scan(&user_name_first)
+		fmt.Printf("Please enter your last name: ")
+		fmt.Scan(&user_name_last)
+		bookings = append(bookings, user_name_first+" "+user_name_last)
 
-	fmt.Printf("Please enter your email address: ")
-	fmt.Scan(&user_email_addr)
+		fmt.Printf("Please enter your email address: ")
+		fmt.Scan(&user_email_addr)
 
-	fmt.Printf("Please enter the number of tickets to book: ")
-	fmt.Scan(&user_tickets)
+		fmt.Printf("Please enter the number of tickets to book: ")
+		fmt.Scan(&user_tickets)
 
-	remaining_tickets -= user_tickets
+		remaining_tickets -= user_tickets
 
-	fmt.Printf(
-		"Successfully booked %v ticket/s for %v. Confirmation sent to %v \n",
-		user_tickets, bookings[0], user_email_addr)
+		fmt.Printf(
+			"Successfully booked %v ticket/s for %v. Confirmation sent to %v \n",
+			user_tickets, bookings[0], user_email_addr)
 
-	fmt.Printf(
-		"%v tickets now remains for %v\n",
-		remaining_tickets, conference_name)
-
-	fmt.Printf("bookings: %v\n", bookings)
+		fmt.Printf("----------\n")
+		fmt.Printf("bookings: %v\n", bookings)
+		fmt.Printf(
+			"%v tickets now remains for %v\n\n",
+			remaining_tickets, conference_name)
+		fmt.Printf("----------\n")
+	}
 }
