556b55c18e8261a69a1203c95ce625151d847afe
oportunitas <taibizzat165@gmail.com>
2024-11-13 11:57:46 +0700

    feat(data-access): find albums by artist

--- data-access/main.go
+++ data-access/main.go
@@ -9,7 +9,40 @@ import (
 	"github.com/go-sql-driver/mysql"
 )
 
+type Album struct {
+	ID     int64
+	Title  string
+	Artist string
+	Price  float32
+}
+
 var db *sql.DB
+
+func albumsByArtist(name string) ([]Album, error) {
+	var albums []Album
+
+	rows, err := db.Query("select * from album where artist = ?", name)
+	if err != nil {
+		return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var alb Album
+		err := rows.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price)
+		if err != nil {
+			return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+		}
+		albums = append(albums, alb)
+	}
+
+	err = rows.Err()
+	if err != nil {
+		return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+	}
+
+	return albums, nil
+}
 
 func main() {
 	cfg := mysql.Config{
@@ -30,5 +63,12 @@ func main() {
 	if ping_err != nil {
 		log.Fatal(ping_err)
 	}
-	fmt.Println("connected!")
+	fmt.Println("connected to recordings mega-library!!! (also called database)")
+
+	albums, err := albumsByArtist("John Coltrane")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("albums found: %v\n", albums)
 }
