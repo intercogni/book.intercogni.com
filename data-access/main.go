d00ccc353a50f4e5ee0ac07315c0f973dc38d944
oportunitas <taibizzat165@gmail.com>
2024-11-13 11:40:52 +0700

    feat(data-access): simple mysql connection

---
+++ data-access/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/go-sql-driver/mysql"
+)
+
+var db *sql.DB
+
+func main() {
+	cfg := mysql.Config{
+		User:   os.Getenv("DBUSER"),
+		Passwd: os.Getenv("DBPASS"),
+		Net:    "tcp",
+		Addr:   "127.0.0.1:3306",
+		DBName: "recordings",
+	}
+
+	var err error
+	db, err = sql.Open("mysql", cfg.FormatDSN())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ping_err := db.Ping()
+	if ping_err != nil {
+		log.Fatal(ping_err)
+	}
+	fmt.Println("connected!")
+}
